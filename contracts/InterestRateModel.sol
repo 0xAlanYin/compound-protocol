d166606ec148b6969cd17926c8bbe58accc772d7
Alanyin <alanyin6@gmail.com>
2024-09-24 11:30:24 +0800

    feat: add InterestRateModel、WhitePaperInterestRateModel code comments

--- contracts/InterestRateModel.sol
+++ contracts/InterestRateModel.sol
@@ -2,29 +2,36 @@
 pragma solidity ^0.8.10;
 
 /**
-  * @title Compound's InterestRateModel Interface
-  * @author Compound
-  */
+ * @title Compound's InterestRateModel Interface
+ * @author Compound
+ */
 abstract contract InterestRateModel {
     /// @notice Indicator that this is an InterestRateModel contract (for inspection)
+    // 使用一个常量来标识这是一个InterestRateModel合约
     bool public constant isInterestRateModel = true;
 
     /**
-      * @notice Calculates the current borrow interest rate per block
-      * @param cash The total amount of cash the market has
-      * @param borrows The total amount of borrows the market has outstanding
-      * @param reserves The total amount of reserves the market has
-      * @return The borrow rate per block (as a percentage, and scaled by 1e18)
-      */
-    function getBorrowRate(uint cash, uint borrows, uint reserves) virtual external view returns (uint);
+     * @notice Calculates the current borrow interest rate per block
+     * @param cash The total amount of cash the market has    市场中的现金总量。这是市场中可用于借款的可用资金。
+     * @param borrows The total amount of borrows the market has outstanding    市场中未偿还的借款总量。这是市场中当前所有未偿还的借款总额。
+     * @param reserves The total amount of reserves the market has    市场中储备的总量。这是市场中保留的资金，通常用于应对市场波动或其他风险。
+     * @return The borrow rate per block (as a percentage, and scaled by 1e18)    借款利率，以每个区块的百分比表示，并按 1e18 缩放。返回值表示每个区块的借款利率。
+     */
+    // 获取借款利率：注意是区块利率，按照每个区块计算利率
+    function getBorrowRate(uint256 cash, uint256 borrows, uint256 reserves) external view virtual returns (uint256);
 
     /**
-      * @notice Calculates the current supply interest rate per block
-      * @param cash The total amount of cash the market has
-      * @param borrows The total amount of borrows the market has outstanding
-      * @param reserves The total amount of reserves the market has
-      * @param reserveFactorMantissa The current reserve factor the market has
-      * @return The supply rate per block (as a percentage, and scaled by 1e18)
-      */
-    function getSupplyRate(uint cash, uint borrows, uint reserves, uint reserveFactorMantissa) virtual external view returns (uint);
+     * @notice Calculates the current supply interest rate per block
+     * @param cash The total amount of cash the market has    市场中的现金总量。这是市场中可用于借款的可用资金。
+     * @param borrows The total amount of borrows the market has outstanding    市场中未偿还的借款总量。这是市场中当前所有未偿还的借款总额。
+     * @param reserves The total amount of reserves the market has    市场中储备的总量。这是市场中保留的资金，通常用于应对市场波动或其他风险。
+     * @param reserveFactorMantissa The current reserve factor the market has    当前市场储备因子。这是一个百分比，表示市场中保留的资金占市场总资金的比例。
+     * @return The supply rate per block (as a percentage, and scaled by 1e18)    存款利率，以每个区块的百分比表示，并按 1e18 缩放。返回值表示每个区块的存款利率。
+     */
+    // 获取存款利率：注意是区块利率，按照每个区块计算利率
+    function getSupplyRate(uint256 cash, uint256 borrows, uint256 reserves, uint256 reserveFactorMantissa)
+        external
+        view
+        virtual
+        returns (uint256);
 }
--- contracts/WhitePaperInterestRateModel.sol
+++ contracts/WhitePaperInterestRateModel.sol
@@ -4,36 +4,41 @@ pragma solidity ^0.8.10;
 import "./InterestRateModel.sol";
 
 /**
-  * @title Compound's WhitePaperInterestRateModel Contract
-  * @author Compound
-  * @notice The parameterized model described in section 2.4 of the original Compound Protocol whitepaper
-  */
+ * @title Compound's WhitePaperInterestRateModel Contract
+ * @author Compound
+ * @notice The parameterized model described in section 2.4 of the original Compound Protocol whitepaper
+ */
+// 直线型的利率模型
+// 借款利率的，其计算公式为：y = k*x + b。 y 即 y 轴的值，即借款利率值，x 即 x 轴的值，表示资金使用率，k 为斜率，b 则是 x 为 0 时的起点值
 contract WhitePaperInterestRateModel is InterestRateModel {
-    event NewInterestParams(uint baseRatePerBlock, uint multiplierPerBlock);
+    event NewInterestParams(uint256 baseRatePerBlock, uint256 multiplierPerBlock);
 
     uint256 private constant BASE = 1e18;
 
     /**
      * @notice The approximate number of blocks per year that is assumed by the interest rate model
      */
-    uint public constant blocksPerYear = 2102400;
+    // 常表示在利率模型中假设的一年内的区块数量。在以太坊网络中，假设每个区块的生成时间大约为 15 秒，因此一年内的区块数量大约为 365 * 24 * 60 * 60 / 15 = 2102400。
+    uint256 public constant blocksPerYear = 2102400;
 
     /**
      * @notice The multiplier of utilization rate that gives the slope of the interest rate
      */
-    uint public multiplierPerBlock;
+    // 区块斜率
+    uint256 public multiplierPerBlock;
 
     /**
      * @notice The base interest rate which is the y-intercept when utilization rate is 0
      */
-    uint public baseRatePerBlock;
+    // 区块级别的基准利率
+    uint256 public baseRatePerBlock;
 
     /**
      * @notice Construct an interest rate model
-     * @param baseRatePerYear The approximate target base APR, as a mantissa (scaled by BASE)
-     * @param multiplierPerYear The rate of increase in interest rate wrt utilization (scaled by BASE)
+     * @param baseRatePerYear The approximate target base APR, as a mantissa (scaled by BASE) 基础年利率
+     * @param multiplierPerYear The rate of increase in interest rate wrt utilization (scaled by BASE) 斜率 k 值
      */
-    constructor(uint baseRatePerYear, uint multiplierPerYear) public {
+    constructor(uint256 baseRatePerYear, uint256 multiplierPerYear) public {
         baseRatePerBlock = baseRatePerYear / blocksPerYear;
         multiplierPerBlock = multiplierPerYear / blocksPerYear;
 
@@ -47,7 +52,8 @@ contract WhitePaperInterestRateModel is InterestRateModel {
      * @param reserves The amount of reserves in the market (currently unused)
      * @return The utilization rate as a mantissa between [0, BASE]
      */
-    function utilizationRate(uint cash, uint borrows, uint reserves) public pure returns (uint) {
+    // 计算资金利用率 `borrows / (cash + borrows - reserves)`
+    function utilizationRate(uint256 cash, uint256 borrows, uint256 reserves) public pure returns (uint256) {
         // Utilization rate is 0 when there are no borrows
         if (borrows == 0) {
             return 0;
@@ -63,9 +69,9 @@ contract WhitePaperInterestRateModel is InterestRateModel {
      * @param reserves The amount of reserves in the market
      * @return The borrow rate percentage per block as a mantissa (scaled by BASE)
      */
-    function getBorrowRate(uint cash, uint borrows, uint reserves) override public view returns (uint) {
-        uint ur = utilizationRate(cash, borrows, reserves);
-        return (ur * multiplierPerBlock / BASE) + baseRatePerBlock;
+    function getBorrowRate(uint256 cash, uint256 borrows, uint256 reserves) public view override returns (uint256) {
+        uint256 ur = utilizationRate(cash, borrows, reserves);
+        return (ur * multiplierPerBlock / BASE) + baseRatePerBlock; // 借款利率 = 资金利用率 * 区块斜率 + 区块基准利率,即 y = kx + b
     }
 
     /**
@@ -76,10 +82,16 @@ contract WhitePaperInterestRateModel is InterestRateModel {
      * @param reserveFactorMantissa The current reserve factor for the market
      * @return The supply rate percentage per block as a mantissa (scaled by BASE)
      */
-    function getSupplyRate(uint cash, uint borrows, uint reserves, uint reserveFactorMantissa) override public view returns (uint) {
-        uint oneMinusReserveFactor = BASE - reserveFactorMantissa;
-        uint borrowRate = getBorrowRate(cash, borrows, reserves);
-        uint rateToPool = borrowRate * oneMinusReserveFactor / BASE;
-        return utilizationRate(cash, borrows, reserves) * rateToPool / BASE;
+    function getSupplyRate(uint256 cash, uint256 borrows, uint256 reserves, uint256 reserveFactorMantissa)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        uint256 oneMinusReserveFactor = BASE - reserveFactorMantissa;
+        uint256 borrowRate = getBorrowRate(cash, borrows, reserves);
+        uint256 rateToPool = borrowRate * oneMinusReserveFactor / BASE;
+        return utilizationRate(cash, borrows, reserves) * rateToPool / BASE;  // 存款利率 = 资金使用率 * 借款利率 *（1 - 储备金率）
+       
     }
 }
